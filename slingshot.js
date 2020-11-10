82e9993c119f029d554cabea6514d2816376a069
SpyGuy <shashankprasanna1@gmail.com>
2020-11-10 18:55:50 -0500

    Made he game

---
+++ slingshot.js
@@ -0,0 +1,31 @@
+class Slingshot {
+	constructor(pointA, bodyB) {
+		//pointA is the hand
+		//BodyB is the rock
+		var options = {
+			pointA: pointA,
+			bodyB: bodyB,
+			length: 20,
+			stiffness: 0.00005,
+		};
+		this.pointA = pointA;
+		this.bodyB = bodyB;
+		this.slingshot = Matter.Constraint.create(options);
+		World.add(world, this.slingshot);
+	}
+	move(body) {
+		Matter.Body.setPosition(body, {x: mouseX, y: mouseY});
+	}
+
+	release() {
+		this.slingshot.pointA = null;
+	}
+
+	display() {
+		if (this.slingshot.pointA != null) {
+			strokeWeight(5);
+			stroke('#000000');
+			line(this.pointA.x, this.pointA.y, this.slingshot.bodyB.position.x, this.slingshot.bodyB.position.y);
+		}
+	}
+}
